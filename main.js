4916c8dd9f376c96daf7e8fbb03c7b60af1650f6
Rashi Ratan <rashiiratan@gmail.com>
2024-10-20 21:51:56 -0400

    Create checkout event and confirmation message

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ sizeSelector.addEventListener("change", (event) => { //adding event listener to 
     selectedSize = sizeSelector.options[sizeSelector.selectedIndex].text; //selecting size text
     updateDropdown(); //to update options for stock availability
     
+    //task 4
+    if (buyButton.disabled === true) {alert('Sorry, this item is out of stock');};
+    //display alert if item out of stock
+
 });//(also corrected comment in html file in this commit)
 
 
@@ -54,3 +58,34 @@ function updateDropdown() //defining function to update dropdown for stock avail
         }
     };
     
+//task 4
+
+//adding event listener for buy button clicked
+buyButton.addEventListener("click", () =>{
+    //redefining selected size for case where user does not change dropdown selection
+    selectedSize = sizeSelector.options[sizeSelector.selectedIndex].text; 
+
+    //creating cases for different sizes selected
+    if (selectedSize.includes("Small")) {
+        smallStock--; //reducing stock
+        alert("Order placed successfully"); //displaying alert
+        checkButton(smallStock); //check button status for next purchase
+    }
+    else if (selectedSize.includes("Medium")) {
+        mediumStock--;
+        alert("Order placed successfully")
+        checkButton(mediumStock);}
+    else if (selectedSize.includes("Large")){
+       largeStock--;
+       alert("Order placed successfully")
+       checkButton(largeStock);
+    }
+    //updating dropdown options per stock availability
+    updateDropdown();
+}
+)
+
+//initial update for dropdown options
+updateDropdown();
+
+//task 5 skipped
